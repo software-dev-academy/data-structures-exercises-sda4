49fb3a355361bc1f623874a9a93f10466ca65783
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-10-15 21:29:27 +0200

    Added JUnit tests for the LinkedList class

---
+++ test/exercise/linkedlist/LinkedListTest.java
@@ -0,0 +1,50 @@
+package exercise.linkedlist;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LinkedListTest {
+
+    private LinkedList linkedList;
+
+    @BeforeEach
+    void setUp() {
+        linkedList = new LinkedList();
+    }
+
+    @AfterEach
+    void tearDown() {
+        linkedList = null;
+    }
+
+    @Test
+    void addValue() {
+        int expected = 105;
+        linkedList.addValue(105);
+        assertEquals(expected, linkedList.getValue(0));
+    }
+
+    @Test
+    void add() {
+        Node expectedNode = new Node(303, null);
+        linkedList.add(expectedNode);
+        assertEquals(expectedNode, linkedList.get(0));
+    }
+
+    @Test
+    void getValue() {
+        int expected = 202;
+        linkedList.addValue(expected);
+        assertEquals(expected,linkedList.getValue(0));
+    }
+
+    @Test
+    void get() {
+        Node expectedNode = new Node(303, null);
+        linkedList.add(expectedNode);
+        assertEquals(expectedNode, linkedList.get(0));
+    }
+}
