a77115486c91068ce1c76bc936b848b07c2886c8
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-10-15 21:10:11 +0200

    Added getters and setters for fields in Node.java
    Added getters and setters for fields in Node.java and changed usage in
    the LinkedList class to match these changes.

--- src/exercise/linkedlist/LinkedList.java
+++ src/exercise/linkedlist/LinkedList.java
@@ -2,78 +2,72 @@ package exercise.linkedlist;
 
 public class LinkedList {
 
-	// storage for elements
-	Node first;
-	
-	public LinkedList() {
-		first = null;
-	}
-	
-	/**
-	 * Adds an integer to the end of this list.
-	 *
-	 * @param i the integer to addValue
-	 */
-	public void addValue(int i) {
-		Node newNode = new Node(i, null);
-		add(newNode);
-	}
+    // storage for elements
+    private Node first;
 
-	/**
-	 * Add a node to the list
-	 *
-	 * @param node Node to be added
-	 */
-	public void add(Node node) {
+    public LinkedList() {
+        first = null;
+    }
 
-		if (first == null) { // list is empty
-			first = node;
-		} else { // list is not empty
-			// 1. step find the last node
-			Node current = first;
+    /**
+     * Adds an integer to the end of this list.
+     *
+     * @param i the integer to addValue
+     */
+    public void addValue(int i) {
+        Node newNode = new Node(i, null);
+        add(newNode);
+    }
 
-			while (current.next != null) {
-				// current is not the last node!
-				current = current.next;
-			}
-			// current.next == null
-			// that means that current is the last node! YAY! :-)
+    /**
+     * Add a node to the list
+     *
+     * @param node Node to be added
+     */
+    public void add(Node node) {
+        if (first == null) { // list is empty
+            first = node;
+        } else { // list is not empty
+            // 1. step find the last node
+            Node current = first;
+            while (current.getNext() != null) {
+                // current is not the last node!
+                current = current.getNext();
+            }
+            // current.next == null
+            // that means that current is the last node! YAY! :-)
+            // 2. step insert new node
+            current.setNext(node);
+        }
+    }
 
-			// 2. step insert new node
-			current.next = node;
-		}
-	}
-
-	/**
-	 * Return element at given index.
-	 * 
-	 * @param index
-	 * @return element at index
-	 * @throws {@link IndexOutOfBoundsException} if index is negative
-	 */
-	public int getValue(int index) {
-		return get(index).elem;
-	}
+    /**
+     * Return element at given index.
+     *
+     * @param index
+     * @return element at index
+     * @throws {@link IndexOutOfBoundsException} if index is negative
+     */
+    public int getValue(int index) {
+        return get(index).getElem();
+    }
 
     /**
      * Get Node at index
+     *
      * @param index Index of Node
-
      * @return {@link Node} at the given index
      */
     public Node get(int index) {
         if (index < 0) {
             throw new IndexOutOfBoundsException();
         }
-
         int i = 0;
         Node current = first;
-
-        while (index != i && current.next != null) {
+        while (index != i && current.getNext() != null) {
             i++;
-            current = current.next;
+            current = current.getNext();
         }
-
         if (index <= i) {
             // here, we are at the right place!
             return current;
@@ -81,23 +75,21 @@ public class LinkedList {
             throw new IndexOutOfBoundsException();
         }
     }
-	
-	public void printAll() {
-		if (first == null) { // list is empty
-			System.out.print("<empty>");
-		} else {
-			// we know that first is not null!
-			System.out.print("[");
-		
-			Node current = first;
-			while (current.next != null) {
-				System.out.print(current.elem + ",");
-				current = current.next;
-			}
-			// print last element
-			System.out.print(current.elem);
-		
-			System.out.print("]");
-		}
-	}
+
+    public void printAll() {
+        if (first == null) { // list is empty
+            System.out.print("<empty>");
+        } else {
+            // we know that first is not null!
+            System.out.print("[");
+            Node current = first;
+            while (current.getNext() != null) {
+                System.out.print(current.getElem() + ",");
+                current = current.getNext();
+            }
+            // print last element
+            System.out.print(current.getElem());
+            System.out.print("]");
+        }
+    }
 }
--- src/exercise/linkedlist/Node.java
+++ src/exercise/linkedlist/Node.java
@@ -1,11 +1,28 @@
 package exercise.linkedlist;
 
-class Node {
-	int elem;
-	Node next;
+public class Node {
 
-	Node(int i, Node n) {
-		elem = i;
-		next = n;
-	}
+    private int elem;
+    private Node next;
+
+    public Node(int i, Node n) {
+        elem = i;
+        next = n;
+    }
+
+    public int getElem() {
+        return elem;
+    }
+
+    public void setElem(int elem) {
+        this.elem = elem;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
