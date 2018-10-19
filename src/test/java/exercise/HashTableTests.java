77018bed5fe651b894a05de38346696bbc82a6c2
Philipp Haller <hallerp@gmail.com>
2018-10-19 10:38:24 +0200

    Fix test

--- src/test/java/exercise/HashTableTests.java
+++ src/test/java/exercise/HashTableTests.java
@@ -11,6 +11,6 @@ public class HashTableTests {
 		HashTable ht = new HashTable();
 		ht.put("kth", 15);
 		int res = (int) ht.get("kth");
-		assertEquals(16, res, "The value that was returned by 'get' did not match the expected value.");
+		assertEquals(15, res, "The value that was returned by 'get' did not match the expected value.");
 	}
 }
