4c5850dc933afdd06aaa2fc3e0c3b8a27a686044
Philipp Haller <hallerp@gmail.com>
2018-10-19 11:47:20 +0200

    Add important documentation to get method

--- src/main/java/exercise/LinkedList.java
+++ src/main/java/exercise/LinkedList.java
@@ -46,7 +46,9 @@ public class LinkedList {
 	}
 
 	/**
-	 * Returns element at given index.
+	 * Returns element at given index. Note that this method throws
+         * an IndexOutOfBoundsException if a negative index is provided, or
+         * if an index larger than size - 1 is provided.
 	 *
 	 * Complexity: O(n)
 	 *
